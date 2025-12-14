59d3f0432f2fb01deac55485f5d3bf04d2d0dcbb
Sami HAMROUNI <samihamrouni282000@gmail.com>
2025-12-14 12:06:40 +0100

    Update stg_fuel_prices.sql

--- models/staging/stg_fuel_prices.sql
+++ models/staging/stg_fuel_prices.sql
@@ -1,27 +1,19 @@
 {{ config(materialized='view') }}
 
-WITH deduplicated AS (
-    SELECT 
-        period,
-        AVG(fuel_price_usd_per_gallon) as fuel_price_usd_per_gallon
-    FROM {{ source('raw', 'fuel_prices') }}
-    GROUP BY period
-)
-
 SELECT 
-    period,
-    fuel_price_usd_per_gallon,
+    date as period,
+    price as fuel_price_usd_per_gallon,
     
     -- Add calculated fields
-    LAG(fuel_price_usd_per_gallon) OVER (ORDER BY period) as previous_price,
-    fuel_price_usd_per_gallon - LAG(fuel_price_usd_per_gallon) OVER (ORDER BY period) as price_change,
+    LAG(price) OVER (ORDER BY date) as previous_price,
+    price - LAG(price) OVER (ORDER BY date) as price_change,
     
     -- Price trend indicators
     CASE 
-        WHEN fuel_price_usd_per_gallon > LAG(fuel_price_usd_per_gallon) OVER (ORDER BY period) THEN 'Increasing'
-        WHEN fuel_price_usd_per_gallon < LAG(fuel_price_usd_per_gallon) OVER (ORDER BY period) THEN 'Decreasing'
+        WHEN price > LAG(price) OVER (ORDER BY date) THEN 'Increasing'
+        WHEN price < LAG(price) OVER (ORDER BY date) THEN 'Decreasing'
         ELSE 'Stable'
     END as price_trend
 
-FROM deduplicated
-ORDER BY period
+FROM {{ source('raw', 'fuel_prices') }}
+ORDER BY date
